4acb6e846349ea112184543d9d77d4363aed82de
Yann Hamon <yann.hamon@contentful.com>
2020-03-22 23:16:08 +0100

    fix integration tests, make them much faster

--- main.go
+++ main.go
@@ -147,12 +147,13 @@ func realMain() int {
 	roleValidator := rolevalidator.NewRoleValidator(arnClient, nsCache)
 
 	r := &controllers.SyncedSecretReconciler{
-		Client:        mgr.GetClient(),
-		Ctx:           ctx,
-		Log:           ctrl.Log.WithName("controllers").WithName("SyncedSecret"),
-		Sess:          session.New(Retry5Cfg),
-		GetSMClient:   smsvcfactory.getSMSVC,
-		RoleValidator: roleValidator,
+		Client:            mgr.GetClient(),
+		Ctx:               ctx,
+		Log:               ctrl.Log.WithName("controllers").WithName("SyncedSecret"),
+		Sess:              session.New(Retry5Cfg),
+		GetSMClient:       smsvcfactory.getSMSVC,
+		RoleValidator:     roleValidator,
+		ReconcileInterval: 120 * time.Second,
 	}
 
 	// Introduce artificial startup delay so that all controllers do not start
